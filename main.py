3c6f0f475da70adfc148c55551877f57be20e57d
Robertas1993 <Entrepreneur0508@gmail.com>
2024-10-07 15:28:09 +0300

    Test

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ class Calculator:
     def __init__(self, symbol: str, number: float) -> None:
         self.symbol = symbol
         self.number = number
+
+    def add(self, other: float) -> float:
+        return self.number + other
+
+    def sub(self, other: float) -> float:
+        return self.number - other
+
+    def div(self, other: float) -> float:
+        if other == 0:
+            raise ValueError("error.")
+        return self.number / other
+
+    def mul(self, other: float) -> float:
+        return self.number * other
+
+    def calculate(self, other: float) -> float:
+        if self.symbol == "+":
+            return self.add(other)
+        elif self.symbol == "-":
+            return self.sub(other)
+        elif self.symbol == "/":
+            return self.div(other)
+        elif self.symbol == "*":
+            return self.mul(other)
+        else:
+            raise ValueError("error: " + self.symbol)
     
-    def add(self) -> float:
-        ...
-
-    def sub(self) -> float:
-        ...
-
-    def div(self) -> float:
-        ...
-
-    def mul(self) -> float:
-        ...
-        
-    def calculate(self) -> float:
-        ...
+    
+ 
